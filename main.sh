56218c703674a0d1c826596d577eacb50dccaa68
Jakub Konkol <kubbo56@gmail.com>
2026-05-13 18:57:43 +0200

    firewalld

--- main.sh
+++ main.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello World"Hello World"
+function check_if_status_active {
+    status=$(systemctl is-active "$1")
+    if [ "$status" == "active" ]; then
+        echo "$1 is active"
+    else
+        echo "$1 is not active"
+    fi
+}
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+check_if_status_active firewalld
+#will finish later
